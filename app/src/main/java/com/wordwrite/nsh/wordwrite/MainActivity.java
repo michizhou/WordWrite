838bded04830b255a5624d58194f55a2ac7dd486
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-09-15 16:30:49 -0400

    Modified most of the app structure to support DialogFlow

---
+++ app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wordwrite.nsh.wordwrite;
+
+//import ai.api.AIListener;
+//import ai.api.android.AIConfiguration;
+//import ai.api.android.AIService;
+//import ai.api.model.AIError;
+//import ai.api.model.AIResponse;
+//import ai.api.model.Result;
+//import com.google.gson.JsonElement;
+//import java.util.Map;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
