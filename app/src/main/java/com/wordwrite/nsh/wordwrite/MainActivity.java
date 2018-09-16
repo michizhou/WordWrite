51f80831c98b11a4021d8de0c7651928d850c246
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-09-15 20:58:12 -0400

    UI config update

--- app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
+++ app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
@@ -6,8 +6,8 @@ package com.wordwrite.nsh.wordwrite;
 //import ai.api.model.AIError;
 //import ai.api.model.AIResponse;
 //import ai.api.model.Result;
-//import com.google.gson.JsonElement;
-//import java.util.Map;
+import com.google.gson.JsonElement;
+import java.util.Map;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
