4e2758a0e825a51bfb10f1c68f1bb7d07d954f7a
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-09-16 01:05:16 -0400

    Added DialogFlow API

--- app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
+++ app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
@@ -1,11 +1,9 @@
 package com.wordwrite.nsh.wordwrite;
 
-//import ai.api.AIListener;
-//import ai.api.android.AIConfiguration;
-//import ai.api.android.AIService;
-//import ai.api.model.AIError;
-//import ai.api.model.AIResponse;
-//import ai.api.model.Result;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import ai.api.AIConfiguration;
 import com.google.gson.JsonElement;
 import java.util.Map;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +11,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.content.Intent;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +19,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Listener function
+        final AIConfiguration config = new AIConfiguration("YOUR_ACCESS_TOKEN",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiButton = (AIButton) findViewById(R.id.micButton);
+
+        aiButton.initialize(config);
+        aiButton.setResultsListener(new AIButton.AIButtonListener() {
+            @Override
+            public void onResult(final AIResponse result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("ApiAi", "onResult");
+                        // TODO process response here
+                    }
+                });
+            }
+
+            @Override
+            public void onError(final AIError error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("ApiAi", "onError");
+                        // TODO process error here
+                    }
+                });
+            }
+        });
+
 
         FloatingActionButton button  = (FloatingActionButton)findViewById(R.id.floatingActionButton);
         button.setOnClickListener(new View.OnClickListener(){
