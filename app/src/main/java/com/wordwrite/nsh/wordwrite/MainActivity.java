f93b57f767be5006fa75aa8cd529b4bbc4f18e0d
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-09-16 02:27:11 -0400

    Integrated API with new UI

--- app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
+++ app/src/main/java/com/wordwrite/nsh/wordwrite/MainActivity.java
@@ -3,7 +3,8 @@ package com.wordwrite.nsh.wordwrite;
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
-import ai.api.AIConfiguration;
+import ai.api.ui.AIButton;
+import ai.api.android.AIConfiguration;
 import com.google.gson.JsonElement;
 import java.util.Map;
 import android.support.v7.app.AppCompatActivity;
@@ -17,19 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // Listener function
         final AIConfiguration config = new AIConfiguration(
                 "14bbcece37504c4aaa16763eee07dc41",
-                AIConfiguration.SupportedLanguages.English);
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
 
-        aiButton = (AIButton) findViewById(R.id.micButton);
+        AIButton aiButton = (AIButton) findViewById(R.id.micButton);
 
         aiButton.initialize(config);
         aiButton.setResultsListener(new AIButton.AIButtonListener() {
-
             @Override
             public void onResult(final AIResponse result) {
                 runOnUiThread(new Runnable() {
@@ -51,14 +50,21 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        });
 
-        FloatingActionButton button  = (FloatingActionButton)findViewById(R.id.floatingActionButton);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent i = new Intent(MainActivity.this, CorrectionActivity.class);
-                startActivity(i);
+            @Override
+            public void onCancelled() {
+                // Something in here
             }
         });
+
+//        FloatingActionButton button  = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+//        button.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                Intent i = new Intent(MainActivity.this, CorrectionActivity.class);
+//                startActivity(i);
+//            }
+//        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
